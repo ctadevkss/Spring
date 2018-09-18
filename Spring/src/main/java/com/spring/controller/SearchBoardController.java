c931a000f223f3d48adda477239a61c440264733
KOSEA-KIMSANGSU <rlatkdtn97@naver.com>
2018-09-18 09:53:33 +0900

    SearchBoardController 0918(1)

--- Spring/src/main/java/com/spring/controller/SearchBoardController.java
+++ Spring/src/main/java/com/spring/controller/SearchBoardController.java
@@ -9,7 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.spring.domain.BoardVO;
 import com.spring.domain.PageMaker;
 import com.spring.domain.SearchCriteria;
 import com.spring.service.BoardService;
@@ -23,6 +26,7 @@ public class SearchBoardController {
 	@Inject
 	private BoardService service;
 	
+	// 게시글 리스트
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public void listPage(@ModelAttribute("criteria") SearchCriteria criteria,
 			             Model model) throws Exception {
@@ -38,6 +42,60 @@ public class SearchBoardController {
 		pageMaker.setTotalCount(service.listSearchCount(criteria)); // 검색조건에 갯수
 		
 		model.addAttribute("pageMaker", pageMaker);
+	}
+	
+	// 상세조회
+	@RequestMapping(value = "/readPage", method = RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, 
+			         @ModelAttribute("criteria") SearchCriteria criteria, 
+			         Model model) throws Exception {
 		
-	}	
+		model.addAttribute(service.read(bno));	
+	}
+	
+	// 삭제
+	@RequestMapping(value = "/removePage", method = RequestMethod.POST)
+	public String remove(@RequestParam("bno") int bno, 
+			             SearchCriteria criteria, 
+			             RedirectAttributes reAttr) throws Exception {
+		
+		service.delete(bno);
+		
+		reAttr.addAttribute("page"      , criteria.getPage());
+		reAttr.addAttribute("perPageNum", criteria.getPerPageNum());
+		reAttr.addAttribute("searchType", criteria.getSearchType());
+		reAttr.addAttribute("keyword"   , criteria.getKeyword());
+		reAttr.addFlashAttribute("msg"  , "success");
+				
+		return "redircet:/sboard/list";
+	}
+	
+	// 수정전
+	@RequestMapping(value = "/modifyPage", method = RequestMethod.GET)
+	public void modifyGET(int bno, 
+			              @ModelAttribute("criteria") SearchCriteria criteria,
+			              Model model) throws Exception {
+		
+		model.addAttribute(service.read(bno));
+	}
+	
+	// 수정
+	@RequestMapping(value = "/modifyPage", method = RequestMethod.POST)
+	public String modifyPOST(BoardVO boardVO, SearchCriteria criteria, 
+			               RedirectAttributes reAttr) throws Exception {
+		
+		logger.info(criteria.toString());
+		service.update(boardVO);
+		
+		reAttr.addAttribute("page"      , criteria.getPage());
+		reAttr.addAttribute("perPageNum", criteria.getPerPageNum());
+		reAttr.addAttribute("searchType", criteria.getSearchType());
+		reAttr.addAttribute("keyword"   , criteria.getKeyword());
+		reAttr.addFlashAttribute("msg"  , "success");
+		
+		logger.info("reAttr.toString(): " + reAttr.toString());
+		
+		return "redircet:/sboard/list";
+	}
+	
 }
